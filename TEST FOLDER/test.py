9402c32ee2e44c80a2041662ab7d7451a8886ffd
mcwiseman97 <mcwiseman97@gmail.com>
2026-03-27 11:13:15 -0500

    feat: implement a word guessing game with random word selection, dynamic hints, and letter-by-letter guess feedback.

--- TEST FOLDER/test.py
+++ TEST FOLDER/test.py
@@ -1,108 +1,42 @@
-import curses
-import time
+import random
 
-def draw_board(stdscr, board):
-    stdscr.clear()
-    height, width = stdscr.getmaxyx()
-    mid_x = width // 2
-    mid_y = height // 2
+word_list = ["computer", "banana", "apple", "orange", "grape"]
 
-    for y in range(len(board)):
-        for x in range(len(board[y])):
-            char = 'X' if board[y][x] == 1 else 'O' if board[y][x] == 2 else '.'
-            stdscr.addch(y + mid_y, x * 3 + mid_x, char)
 
-    stdscr.refresh()
+random_word = random.choice(word_list)
+correct_length = len(random_word)
 
-def get_move(stdscr):
-    while True:
-        key = stdscr.getch()
-        if key in [curses.KEY_LEFT, curses.KEY_RIGHT]:
-            return key
-        elif key == ord('q'):
-            return None
 
-def check_winner(board):
-    rows = len(board)
-    cols = len(board[0])
-    player1 = 1
-    player2 = 2
+print("Welcome to the word guessing game!")
+print("Your hint is:", end=" ")
+for i in range(correct_length):
+    print("_ ", end="")
 
-    # Check horizontal
-    for y in range(rows):
-        for x in range(cols - 3):
-            if all(board[y][x + i] == player1 for i in range(4)):
-                return player1
-            elif all(board[y][x + i] == player2 for i in range(4)):
-                return player2
+guess = ""
+guess_count = 0
 
-    # Check vertical
-    for x in range(cols):
-        for y in range(rows - 3):
-            if all(board[y + i][x] == player1 for i in range(4)):
-                return player1
-            elif all(board[y + i][x] == player2 for i in range(4)):
-                return player2
+while guess != random_word:
+    guess = input("\nWhat is your guess? ")
+    guess_length = len(guess)
+    guess_count = guess_count + 1
+    if guess_length != correct_length:
+        print(f"Sorry, the guess must have {correct_length} letters.")
+        continue #continue indicates that we can now skip current loop and move on to the next loop
+    
+    #l_list means letter list (For results of the guess)
+    l_list = [""] * correct_length
 
-    # Check diagonal (top-left to bottom-right)
-    for y in range(rows - 3):
-        for x in range(cols - 3):
-            if all(board[y + i][x + i] == player1 for i in range(4)):
-                return player1
-            elif all(board[y + i][x + i] == player2 for i in range(4)):
-                return player2
-
-    # Check diagonal (top-right to bottom-left)
-    for y in range(rows - 3):
-        for x in range(3, cols):
-            if all(board[y + i][x - i] == player1 for i in range(4)):
-                return player1
-            elif all(board[y + i][x - i] == player2 for i in range(4)):
-                return player2
-
-    # Check for draw
-    if any('.' in row for row in board):
-        return None
-    else:
-        return 0
-
-def main(stdscr):
-    curses.curs_set(0)  # Hide the cursor
-    stdscr.nodelay(1)   # Make getch non-blocking
-    stdscr.timeout(100)  # Refresh every 100ms
-
-    rows, cols = 6, 7
-    board = [['.' for _ in range(cols)] for _ in range(rows)]
-    current_player = 1
-
-    while True:
-        draw_board(stdscr, board)
-        move = get_move(stdscr)
-
-        if move is None:
-            break
-
-        if move == curses.KEY_LEFT:
-            # Move left
-            pass
-        elif move == curses.KEY_RIGHT:
-            # Move right
-            pass
+    for i in range(correct_length):
+        if guess[i] == random_word[i]:
+            l_list[i] = guess[i].upper()
+        elif guess[i] in random_word:
+            l_list[i] = guess[i].lower()
         else:
-            # Drop piece
-            for y in range(rows - 1, -1, -1):
-                if board[y][current_player - 1] == '.':
-                    board[y][current_player - 1] = current_player
-                    break
+            l_list[i] = "_"
+    
+    for letter in l_list:
+        print(letter, end=" ") # using " " instead of "" allows for there to be a space between letters
+    print()
 
-            winner = check_winner(board)
-            if winner is not None:
-                draw_board(stdscr, board)
-                stdscr.addstr(7, 0, f"Player {winner} wins!")
-                stdscr.refresh()
-                time.sleep(2)
-                break
-
-        current_player = 3 - current_player  # Switch player
-
-curses.wrapper(main)
+print("Congratulations! You guessed it!")
+print(f"It took you {guess_count} guesses.")
--- W04/W04WordPuzzle.py
+++ W04/W04WordPuzzle.py
@@ -1,44 +1,31 @@
 """
 Author: Michael Wiseman
 Assignment:
-date: 3/24/26
+date: 3/27/26
 """
 
-# Wordle Style Game
-
 """
-Criteria
-An underscore _ indicates that the letter was not present in the secret word.
-
-A lowercase letter indicates that the letter was present somewhere in the secret word, but not at that position.
-
-An uppercase letter indicates that the letter was present at that exact spot in the secret word. (In other words, if the second letter in the guess is also the second letter in the secret word, then that letter is shown as a capital.)
-
-
-MIDWEEK EXPECTATIONS
-1. Have a secret word stored in the program.
-
-2. Prompt the user for a guess.
-
-3. Continue looping as long as that guess is not correct.
-
-4. Calculate the number of guesses and display it at the end.
-
+Used import random
+I added to this assignment a list of words and a randomizer so that each time a player starts, they will have to figure out the new word.
+With this, i made the program check the length of the word to dynamically show the correct hint length.
 """
+import random
 
-correct_word = "computer"
-correct_length = len(correct_word)
+word_list = ["computer", "banana", "apple", "orange", "grape"]
 
+random_word = random.choice(word_list)
+correct_length = len(random_word)
 
 print("Welcome to the word guessing game!")
-print()
-print("Your hint is: _ _ _ _ _ _ _ _")
+print("Your hint is:", end=" ")
+for i in range(correct_length):
+    print("_ ", end="")
 
 guess = ""
 guess_count = 0
 
-while guess != correct_word:
-    guess = input("What is your guess? ")
+while guess != random_word:
+    guess = input("\nWhat is your guess? ")
     guess_length = len(guess)
     guess_count = guess_count + 1
     if guess_length != correct_length:
@@ -49,9 +36,9 @@ while guess != correct_word:
     l_list = [""] * correct_length
 
     for i in range(correct_length):
-        if guess[i] == correct_word[i]:
+        if guess[i] == random_word[i]:
             l_list[i] = guess[i].upper()
-        elif guess[i] in correct_word:
+        elif guess[i] in random_word:
             l_list[i] = guess[i].lower()
         else:
             l_list[i] = "_"
@@ -62,57 +49,3 @@ while guess != correct_word:
 
 print("Congratulations! You guessed it!")
 print(f"It took you {guess_count} guesses.")
-
-
-
-
-
-
-"""
-ORIGINAL ATTEMPT
-print("Welcome to the word guessing game!")
-print()
-
-correct_word = "computer"
-correct_word_length = len(correct_word)
-
-trigger = True
-
-guess = ""
-guess_count = 0
-
-guess = input("What is your guess? ").lower()
-
-guess_length = len(guess)
-
-
-if guess_length != correct_word_length:
-    print("Your word is not the right legth. Try again")
-else:
-    
-    i = 0
-    # Lines up the index to the corrisponding appleletter in correct word
-    for index in range(correct_word_length):
-        print(index, end="")
-        print(correct_word[i], end="")
-        print(guess[i])
-        i = i + 1
-
-
-    print()
-
-    # Verifying matches - Displays matches in the correct index
-    i = 0
-    while i < 8:
-        if correct_word[i] == guess[i]:
-            print(f"{correct_word[i].upper()} ", end="")
-            i = i +1
-        else:
-            print("_ ", end="")
-            i = i + 1
-    print()
-
-
-# return index of matching letters in wrong locations
-
-"""
